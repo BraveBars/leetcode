d57e32b739a612d62d5e590145d8df3525475778
Barsukov Sergey <BraveBars@gmail.com>
2026-05-05 15:00:13 +0300

    feat(strings): 1768 Merge Strings Alternately

---
+++ src/main/java/com/leetCode75/ArrayString/MergeStringsAlternately.java
@@ -0,0 +1,55 @@
+//You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+//
+//Return the merged string.
+//
+//Example 1:
+//Input: word1 = "abc", word2 = "pqr"
+//Output: "apbqcr"
+//Explanation: The merged string will be merged as so:
+//word1:  a   b   c
+//word2:    p   q   r
+//merged: a p b q c r
+//
+//Example 2:
+//Input: word1 = "ab", word2 = "pqrs"
+//Output: "apbqrs"
+//Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+//word1:  a   b
+//word2:    p   q   r   s
+//merged: a p b q   r   s
+//
+//Example 3:
+//Input: word1 = "abcd", word2 = "pq"
+//Output: "apbqcd"
+//Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+//word1:  a   b   c   d
+//word2:    p   q
+//merged: a p b q c   d
+//
+//Constraints:
+//1 <= word1.length, word2.length <= 100
+//word1 and word2 consist of lowercase English letters.
+package com.leetCode75.ArrayString;
+
+public class MergeStringsAlternately {
+    public static void main(String[] args) {
+        System.out.println(mergeAlternately("abc", "pqr"));
+        System.out.println(mergeAlternately("ab", "pqrs"));
+        System.out.println(mergeAlternately("abcd", "pq"));
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder(word1.length() + word2.length());
+        int counter = 0;
+        while (counter < word1.length() || counter < word2.length()) {
+            if (counter < word1.length()) {
+                result.append(word1.charAt(counter));
+            }
+            if (counter < word2.length()) {
+                result.append(word2.charAt(counter));
+            }
+            counter++;
+        }
+        return result.toString();
+    }
+}
